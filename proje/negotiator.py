da11f70403d772c1b30f413bd017579564307e4a
Kadir Emin İslamoğlu <keislamoglu@yandex.com>
2015-12-27 20:13:10 +0200

    mantik hatalari duzeltildi
    dokumanin yanlis anlasilmasindan kaynakli yapilan hatalar duzeltildi

--- proje/negotiator.py
+++ proje/negotiator.py
@@ -11,7 +11,6 @@ TYPE_NEGOTIATOR = 'N'
 TYPE_PEER = 'P'
 STATE_WAITING = 'W'
 STATE_SUCCESS = 'S'
-STATE_NOT_RESPONDING = 'NR'
 PATCH_SIZE = 128
 UPDATE_INTERVAL = 600
 THREADNUM = 4
@@ -36,7 +35,7 @@ def main():
         checking_queue = Queue.Queue()
         thread_queue = Queue.Queue()
         # Client Side Thread
-        ClientSideThread(checking_queue, s, client_socket, client_address, connectPointList)
+        ClientSideThread(checking_queue, s, connectPointList)
         # Server Side Listener Thread
         ServerSideListenerThread(client_socket, client_address, thread_queue, checking_queue, connectPointList)
         # Server Side Sender Thread
@@ -53,42 +52,50 @@ class ServerSideListenerThread(threading.Thread):
         self.caddress = client_address
         self.thread_queue = thread_queue
         self.connect_point_list = connect_point_list
+        self.__connect_point = False
 
     def run(self):
         while True:
-            received_msg = self.csocket.recv(1024)
+            received_msg = str(self.csocket.recv(1024)).strip(' ')
             # socket baglantisinda hata olustugunda bos string gonderir, dolayisiyla thread'leri sonlandiririz
             if received_msg == '':
                 self.put_queue(CONNECTION_LOST)
                 break
-
             # komut ve arguman ayristiriliyor
-            cmd, argument = str(received_msg).split(' ', 1)
+            if len(str(received_msg).split(' ', 1)) == 2:
+                cmd, argument = str(received_msg).split(' ', 1)
+            else:
+                cmd = received_msg
+                argument = False
 
             # komut formati dogru degilse komut hatali mesaji gonder
             if not len(cmd) == 5:
                 to_queue = 'CMDER'
+            # Kayit yapilmadiysa kayit talebi disindaki komutlar icin "REGER" hatasi verir
+            elif cmd != 'REGME' and (
+                not self.__connect_point or get_node_status(self.__connect_point) != STATE_SUCCESS):
+                to_queue = 'REGER'
             # Kayit talebi
             elif cmd == 'REGME':
-                connection_point = str(argument).split(':', 1)
-
+                self.__connect_point = str(argument).split(':', 1)
                 # ip ve port numaralari formatlari dogrulandiysa devam eder
-                if is_valid_ip_port(connection_point):
+                if is_valid_ip_port(self.__connect_point):
                     t = time.ctime()
-                    # (<ip>, <port>): (<state>, <type>, <time>) olarak node_list'e kaydediliyor
-                    self.connect_point_list.update({(ip, port): (STATE_WAITING,)})
-                    if self.reverse_checking():
-                        t = time.time()
-                        state, type = self.connect_point_list[(ip, port)]
-                        self.connect_point_list.update({(ip, port): (state, type, t)})
-                        to_queue = 'REGOK %s' % t
-                    else:
-                        to_queue = CONNECTION_CLOSED
+                    # (<ip>, <port>): (<state>, <time>, <type>) olarak node_list'e kaydediliyor
+                    self.connect_point_list.update({self.__connect_point: (STATE_WAITING,)})
+                    to_queue = 'REGWA'
+                    self.reverse_checking()
                 else:
-                    to_queue = 'CMDER'
+                    to_queue = CONNECTION_CLOSED
+            elif cmd == 'GETNL':
+                if argument:
+                    to_queue = prepare_node_list(argument)
+                else:
+                    to_queue = prepare_node_list()
+            else:
+                to_queue = 'CMDER'
 
             self.put_queue(to_queue)
-
             # baglanti sona erdiginde thread sonlandirilir
             if to_queue == CONNECTION_CLOSED:
                 break
@@ -98,13 +105,7 @@ class ServerSideListenerThread(threading.Thread):
         self.thread_queue.put(data)
 
     def reverse_checking(self):
-        self.put_queue('REGWA')
-        self.checking_queue.put("CHECK")
-        while self.connect_point_list[(ip, port)] == STATE_WAITING:
-            continue
-        if self.connect_point_list[(ip, port)] == STATE_NOT_RESPONDING:
-            self.put_queue('REGER')
-            return False
+        self.checking_queue.put("CHECK %s:%s" % self.__connect_point)
 
 
 # Server Side Sender Thread
@@ -156,13 +157,10 @@ class ClientSideThread(threading.Thread):
                     connect_point = str(queue_message[6:]).split(':')
                     self.own_socket.connect(connect_point)
                     self.own_socket.send("HELLO")
-                    self.own_socket.settimeout(10.0)
                     response = self.own_socket.recv(1024)
-                    if response[0:5] == 'SALUT':
-                        type = response[6:1]
-                        self.connect_point_list.update({connect_point: (STATE_SUCCESS, type)})
-                    else:
-                        self.connect_point_list.update({connect_point: (STATE_NOT_RESPONDING,)})
+                    # SALUT cevabi ve type gonderildiginde connect_point_list'te guncellenir
+                    if response[0:5] == 'SALUT' and is_valid_type(response[6:1]):
+                        self.connect_point_list.update({connect_point: (STATE_SUCCESS, time.time(), response[6:1])})
 
 
 # --------------------
@@ -190,3 +188,30 @@ def is_valid_ip_port((ip, port)):
 #
 def is_valid_type(type):
     return type == TYPE_NEGOTIATOR or type == TYPE_PEER
+
+
+#
+# connect_point_list'i string bir protokol ifadesine donusturur
+#
+def prepare_node_list(nlsize=-1):
+    cpl = connectPointList
+    cpl_str = 'NLIST BEGIN\n'
+    for cp in cpl:
+        if cpl[cp][0] == STATE_SUCCESS:
+            cpl_str += '%s:%s\n' % (':'.join(cp), ':'.join((cpl[cp][1], cpl[cp][2])))
+        nlsize -= 1
+        if nlsize == 0:
+            break
+    cpl_str += 'NLIST END'
+    return cpl_str
+
+
+#
+# connect_point parametresi ile node un mevcut olup olmadigi sorgulanir
+#
+def get_node_status(connect_point):
+    return connectPointList[connect_point][0]
+
+
+def node_exists(connect_point):
+    return connectPointList.has_key(connect_point)
